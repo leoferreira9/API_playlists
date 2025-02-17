dbfe22ec07d4166bcd2f852d34f6b75f9d804a74
Leonardo <leobarros_@outlook.com>
2025-02-16 23:12:41 -0300

    corrigindo acesso a porta do servidor

--- src/server.js
+++ src/server.js
@@ -7,5 +7,5 @@ const app = express();
 app.use(express.json());
 app.use("/api", router);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor iniciado na URL: http://localhost:${PORT}`));
